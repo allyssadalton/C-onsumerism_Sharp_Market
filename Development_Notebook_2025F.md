b96542157717729fd5c7b482c3f861ad8c8c779c
Allyssa Dalton <allyssamarie06@icloud.com>
2025-11-03 15:59:11 -0500

    needs testing and a few more things

--- Development_Notebook_2025F.md
+++ Development_Notebook_2025F.md
@@ -23,11 +23,11 @@ Build a CSharp console game.
 
 - **Iteration 1:**  
   - **Goal/Task/Rationale:**  
-      Create an introduction screen
+      Create an introduction screen.
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       I wrote the introduction paragraph to explain the game and controls
+       I wrote the introduction paragraph to explain the game and controls. When you press any key, it starts the game.
       
 - **Response/Result:**
 Finished intro paragraph 
@@ -39,142 +39,140 @@ Done
 
 - **Iteration 2:**  
   - **Goal/Task/Rationale:**  
-      
+      Create a method for the stats UI
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created a that clears the console, then shows all player stats
       
 - **Response/Result:**
 
-
+    Finished stats UI
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
-
+Done
 
 - **Iteration 3:**  
   - **Goal/Task/Rationale:**  
-      
+      Create a UpdatePrice method to update the price of stocks.
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created a method for the stock prices. There are random events for stock crashes and spikes.
       
 - **Response/Result:**
-
+    Method suffcessfully updated stock prices
 
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
 
-
+Done 
 
 - **Iteration 4:**  
   - **Goal/Task/Rationale:**  
-      
+      Pause Method
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created a method that when the esc key is pressed, the game is paused. I also added a Thread.Sleep() method to prevent cpu from overworking
       
 - **Response/Result:**
-
+Game can now be paused
 
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
-
+Done
 
 
 
 - **Iteration 5:**  
   - **Goal/Task/Rationale:**  
-      
+      Create reset method
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created a method that restarts the game by resetting all the variables to their original state and redisplays the introduction paragraph
       
 - **Response/Result:**
-
+    You can restart the game
 
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
 
-
+Done
 
 
 - **Iteration 6:**  
   - **Goal/Task/Rationale:**  
-      
+      Create end screen method
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created an end screen method that displays your final stats.
       
 - **Response/Result:**
+    Game now has end state
 
-
-  
+    
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
-
+Current ethod is done but I want to add some type of memory or somehting so that you can restart and try to beat your previous scores.
 
 
 
 - **Iteration 7:**  
   - **Goal/Task/Rationale:**  
-      
+      Create prev game memory
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I added variables to keep track of the previous game stats and i created a prevGame boolean variable to check to see if there was a previous game to compare the current stats to 
       
 - **Response/Result:**
-
-
+    You can try to beat your previous networth
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
-
+Done but I would like to keep trakc of high scores, if I have the time.
 
 
 
 - **Iteration 8:**  
   - **Goal/Task/Rationale:**  
-      
+      Changed the time to start at 7 am
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
        
-      
+      I changed time to be 420 at the start, and I added an if/else statement to show wether it's am or pm. 
 - **Response/Result:**
 
-
+Time displays prettier
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
 
-
-
+Done
 
 - **Iteration 9:**  
   - **Goal/Task/Rationale:**  
-      
+      Allow for command line interaction for the game
       
   - **What do you do?**   
     {If you ask AI, provide your prompt and link. If you fix it yourself, describe how you do it.}  
-       
+       I created a switch statement for different key pressed and now the cooresponding metnhods are called
       
 - **Response/Result:**
-
+Game is playable.
 
   
 
 - **Your Evaluation:** {Issues/errors/your decision:done/discard/revise prompt}
-
+Complete but need to add error catching for non number entries
 
 
 
--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 using System;
 
+using System.Threading;
+
 class Program{
-    static bool running = true;
-    static bool paused = false;
+    static bool gameRunning = false;
+    static bool gamePaused = false;
     static int price = 50;
     static int cash = 1000;
-    static int stock = 0;
-    static int timeLeft = 540; // 9 minutes 
+    static int stocks = 0;
+    static int time = 420; //7am
+    static bool prevGame = false;
     static Random rand = new Random();
-    
-    
-    static void Main(string[] args){
+    static int prevCash, prevStocks, prevNetWorth;
+    static int num;
+
+    static void IntroScreen()
+    {
+        Console.Clear();
         Console.WriteLine(@"
-                                                    Welcome to C(onsumerism) Sharp Market! 
+                                                        Welcome to C(onsumerism) Sharp Market!
+                            --------------------------------------------------------------------------------------------------
                                         You are a stock market trader watching live stock market prices.
                                 Your goal is to maximize profit before the trading day ends at 4pm without insider trading.
                                                         Prices change every few seconds. 
@@ -26,11 +33,188 @@ class Program{
                                                                     R - Restart
                                                                     ESC - Pause
                                                                       Q - Quit
+                                                                      
+                                                      Press any key to open up Sharp Street
                                                               
         ");
-        
+        while (true)
+        {
+            if (Console.KeyAvailable) { break; }
+        }
+    }
+
+    static void StatsUI(){
+        int netWorth = cash + (stocks * price);
+        Console.Clear();
+        Console.WriteLine("[B] - Buy [S] - Sell [N] - Skip/Wait One Tick [R] - Restart [ESC] - Pause [Q] - Quit");
+        Console.WriteLine("----------------------------------------");
+        if (time >= 720){
+            if (time < 780) { Console.WriteLine($"Time: 	12:{time % 60:D2}PM"); }
+            else { Console.WriteLine($"Time: {(time / 60 % 12)}:{time % 60:D2}PM"); }
+        }
+        else { Console.WriteLine($"Time: {(time / 60 % 12)}:{time % 60:D2}AM"); }
+        Console.WriteLine($"Money: ${cash}");
+        Console.WriteLine($"Stocks Owned: {stocks}");
+        Console.WriteLine($"Current Price: %{price}");
+        Console.WriteLine($"Net Worth: ${netWorth}");
+        Console.WriteLine("----------------------------------------");
+    }
+
+    static void UpdateStockPrice(){
+        int change = rand.Next(-10, 11);
+        price += change;
+        if (price < 1) { price = 1; }
+        int chaoticEventChance = rand.Next(1, 20);
+        if (chaoticEventChance == 1) {
+            int spike = rand.Next(20, 40);
+            price += spike;
+            Console.WriteLine($"Stock Prices Spiked!! They increased by ${spike}.");
+        }
+
+        else if (chaoticEventChance == 2) {
+            int drop = rand.Next(20, 40);
+            price -= drop;
+            Console.WriteLine($"Stock Prices Crashed!! They decreased by ${drop}.");
+        }
+        else {Console.WriteLine($"Stock prices are now {price}.");}
+    }
+
+    static void PauseScreen() {
+        gamePaused = true;
+        Console.WriteLine("Game Paused. Press ESC to resume.");
+        while (true){
+            if (Console.KeyAvailable){
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); 
+                if (keyInfo.Key == ConsoleKey.Escape){
+                    gamePaused = false;
+                    StatsUI();
+                    break;
+                }
+            }
+            Thread.Sleep(100);
+        }
+    }
+
+     static void ResetGame(){
+        price = 50;
+        cash = 1000;
+        stocks = 0;
+        time = 420;
+        IntroScreen();
+    }
+
+    static void EndScreen(){
+        int finalWorth = cash + (stocks * price);
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("Time: 4:00pm");
+        Console.WriteLine("Sharp Street is now closed.");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine($"Final Cash: ${cash}");
+        Console.WriteLine($"Stocks Owned: {stocks}");
+        Console.WriteLine($"Net Worth: ${finalWorth}");
+
+        if (prevGame){
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Previous Attempt Stats:");
+            Console.WriteLine($"Final Cash: ${prevCash}");
+            Console.WriteLine($"Stocks Owned: {prevStocks}");
+            Console.WriteLine($"Net Worth: ${prevNetWorth}");
+
+            if (prevNetWorth > finalWorth){
+                Console.WriteLine("You did not beat your previous Final Net Worth.");
+                Console.WriteLine("Booooooo!!!!! Maybe next time. :(");
+                Console.WriteLine("----------------------------------------");
+            }
+
+            else{
+                Console.WriteLine("You did beat your previous Final Net Worth!!!!!!");
+                Console.WriteLine("Great job!!!!! I'm so proud of you. You're becoming the best stock buying in the world!!!!!");
+                Console.WriteLine("----------------------------------------");
+            }
+        }
+
+        Console.WriteLine($"Press R to play again and try to beat your net worth or Q to quit.");
+        Console.WriteLine("----------------------------------------");
+
+        while (true){
+            var key = Console.ReadKey(true).Key;
+            if (key == ConsoleKey.R) {
+                prevGame = true;
+                prevCash = cash;
+                prevStocks = stocks;
+                prevNetWorth = finalWorth;
+                ResetGame();
+                break;
+            }
+            else if (key == ConsoleKey.Q) {
+                gameRunning = false;
+                break;
+            }
+        }
+    }
+
+
+    static void Main(string[] args){
+        IntroScreen();
+        StatsUI();
+        gameRunning = true;
+
         while (gameRunning){
-            
+            Thread.Sleep(2000);
+            time += 2;
+
+            UpdateStockPrice();
+            StatsUI();
+             
+            if (time >= 960){
+                EndScreen();
+                continue;
+            }
+            if (Console.KeyAvailable) {
+                switch (Console.ReadKey(true).Key) {
+                    case ConsoleKey.Escape:
+                        PauseScreen();
+                        break;
+                    case ConsoleKey.R:
+                        ResetGame();
+                        break;
+                    case ConsoleKey.Q:
+                        EndScreen();
+                        break;
+                    case ConsoleKey.B:
+                        Console.WriteLine("How many stocks would you like to buy: ");
+                        num = int.Parse(Console.ReadLine());
+                        if (cash >= num * price) {
+                            cash -= num * price;
+                            stocks += num;
+                        }
+                        else {
+                            num = price / cash;
+                            Console.WriteLine($"You only have enough cash for {num} stocks.");
+                            cash -= num * price;
+                            stocks += num;
+                        }
+                        Console.WriteLine($"Successfully bought {num} stocks for {price} each!");
+                        break;
+
+                    case ConsoleKey.S:
+                        Console.WriteLine("How many stocks would you like to sell: ");
+                        num = int.Parse(Console.ReadLine());
+                        if (stocks >= num) {
+                            cash += num * price;
+                            stocks -= num;
+                        }
+                        else {
+                            num = stocks;
+                            Console.WriteLine($"You only have {stocks} stocks.");
+                            cash += num * price;
+                            stocks = 0;
+                        }
+                        Console.WriteLine($"Successfully sold {num} stocks for {price} each!");
+                        break;
+                }
+            }
+            Thread.Sleep(100); 
         }
     }
 }
